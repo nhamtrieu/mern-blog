b81f3d05a577eb0fd5e4c25c6fd690f6e291dbec
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2024-01-16 15:00:06 +0700

    create pages and add routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 function App() {
     return (
-        <>
-            <div>
-                Hello world! <br />
-            </div>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-up" element={<Signup />} />
+                <Route path="/dashboard/*" element={<Dashboard />} />
+                <Route path="projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
