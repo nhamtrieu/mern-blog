c505ad83ca8fac8b0dd688db097504d8aef8d408
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2024-01-16 23:37:32 +0700

    create header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
+
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
