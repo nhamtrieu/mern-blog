89729a9a02384f75d64ea0a408d1d64e257ddce6
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2024-01-22 13:49:16 +0700

    add update post

--- back-end/routes/post.route.js
+++ back-end/routes/post.route.js
@@ -4,6 +4,7 @@ import {
     create,
     deletepost,
     getposts,
+    updatepost,
 } from "../controllers/post.controller.js";
 
 const route = express.Router();
@@ -11,5 +12,6 @@ const route = express.Router();
 route.post("/create", verifyToken, create);
 route.get("/getposts", getposts);
 route.delete("/delete/:postId/:userId", verifyToken, deletepost);
+route.put("/update/:postId/:userId", verifyToken, updatepost);
 
 export default route;
