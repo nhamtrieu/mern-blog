100b64d056ff059421e46b2cb0eb5ff896819094
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2024-01-22 13:27:15 +0700

    add delete post

--- back-end/routes/post.route.js
+++ back-end/routes/post.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create, getposts } from "../controllers/post.controller.js";
+import {
+    create,
+    deletepost,
+    getposts,
+} from "../controllers/post.controller.js";
 
 const route = express.Router();
 
 route.post("/create", verifyToken, create);
 route.get("/getposts", getposts);
+route.delete("/delete/:postId/:userId", verifyToken, deletepost);
 
 export default route;
--- front-end/src/components/DashPosts.jsx
+++ front-end/src/components/DashPosts.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
-import { Table } from "flowbite-react";
+import { Modal, Table, Button } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function DashPosts() {
     const { currentUser } = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState([]);
     const [showMore, setShowMore] = useState(true);
+    const [showModal, setShowModal] = useState(false);
+    const [postIdToDelete, setPostIdToDelete] = useState("");
     useEffect(() => {
         const getPost = async () => {
             try {
@@ -38,6 +41,20 @@ export default function DashPosts() {
             if (res.data.posts.length < 9) {
                 setShowMore(false);
             }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const handleDeletePost = async () => {
+        setShowModal(false);
+        try {
+            const res = await axios.delete(
+                `/api/v1/post/delete/${postIdToDelete}/${currentUser._id}`
+            );
+            console.log(res);
+            setUserPosts((prev) =>
+                prev.filter((post) => post._id !== postIdToDelete)
+            );
         } catch (error) {
             console.log(error);
         }
@@ -84,7 +101,13 @@ export default function DashPosts() {
                                     </Table.Cell>
                                     <Table.Cell>{post.category}</Table.Cell>
                                     <Table.Cell>
-                                        <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                                        <span
+                                            onClick={() => {
+                                                setPostIdToDelete(post._id);
+                                                setShowModal(true);
+                                            }}
+                                            className="font-medium text-red-500 hover:underline cursor-pointer"
+                                        >
                                             Delete
                                         </span>
                                     </Table.Cell>
@@ -112,6 +135,30 @@ export default function DashPosts() {
             ) : (
                 <p>You have no post yet</p>
             )}
+            <Modal
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                popup
+                size={"md"}
+            >
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="h-14 w-14 to-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                        <h3 className="mb-5 text-xl text-gray-500 dark:text-gray-200">
+                            Are you sure you want to delete this post
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color="failure" onClick={handleDeletePost}>
+                                Yes, I'm sure
+                            </Button>
+                            <Button onClick={() => setShowModal(false)}>
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
