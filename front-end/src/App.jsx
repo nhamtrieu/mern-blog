7fba7bae55e7de755b28e33c1a5578b1951dd961
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2024-01-25 02:10:00 +0700

    add post page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 function App() {
     return (
@@ -32,7 +33,8 @@ function App() {
                         element={<UpdatePost />}
                     />
                 </Route>
-                <Route path="projects" element={<Projects />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/post/:postSlug" element={<PostPage />} />
             </Routes>
             <FooterComp />
         </BrowserRouter>
